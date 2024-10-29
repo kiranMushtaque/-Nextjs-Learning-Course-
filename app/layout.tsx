0431b67cba29f69724bc7832b842db4287a7eda1
kiranMushtaque <kiranmushtaque373@gmail.com>
2024-10-29 17:33:25 +0500

    layout update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,22 @@
 
 
 
-// app/layout.tsx
-import "@/app/ui/global.css";
-import { inter } from "@/app/ui/fonts"; // Yeh sahi import hai
-import { ReactNode } from "react"; // Import ReactNode
 
-interface RootLayoutProps {
-  children: ReactNode; // Define children as ReactNode
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts"; 
+import { ReactNode } from "react"; 
+
+interface  RootLayoutProps {
+  children: ReactNode; 
+
+   
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <body style={{ fontFamily: inter.fontFamily }}>{children}</body>
+
+      <body style={{ fontFamily: inter.fontFamily }}>{children} </body>
     </html>
   );
 }
